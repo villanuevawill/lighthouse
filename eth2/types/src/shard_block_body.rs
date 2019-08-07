1f37cd5d8b6ad5826c132c4f7357cdd07fb65f98
William Villanueva <wvillanu89@gmail.com>
2019-08-07 13:40:00 +1000

    Include a number of important shard types

---
+++ eth2/types/src/shard_block_body.rs
@@ -0,0 +1,36 @@
+use crate::test_utils::{graffiti_from_hex_str, TestRandom};
+use crate::*;
+
+use serde_derive::{Deserialize, Serialize};
+use ssz_derive::{Decode, Encode};
+use test_random_derive::TestRandom;
+use tree_hash_derive::{CachedTreeHash, TreeHash};
+
+/// The body of a `BeaconChain` block, containing operations.
+///
+/// Spec v0.6.3
+#[derive(
+    Debug,
+    PartialEq,
+    Clone,
+    Serialize,
+    Deserialize,
+    Encode,
+    Decode,
+    TreeHash,
+    CachedTreeHash,
+    TestRandom,
+)]
+pub struct ShardBlockBody {
+    #[serde(deserialize_with = "graffiti_from_hex_str")]
+    pub graffiti: [u8; 32],
+    pub attestations: Vec<ShardAttestation>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    ssz_tests!(BeaconBlockBody);
+    cached_tree_hash_tests!(BeaconBlockBody);
+}
