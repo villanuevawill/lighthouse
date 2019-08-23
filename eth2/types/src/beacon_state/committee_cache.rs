8aa4e4af71ebb3aaf261b542334ca42f671a55a0
William Villanueva <wvillanu89@gmail.com>
2019-08-23 11:47:47 +0200

    Removed specific shard committee logic and pass through an argument for the period committee

--- eth2/types/src/beacon_state/committee_cache.rs
+++ eth2/types/src/beacon_state/committee_cache.rs
@@ -18,7 +18,6 @@ pub struct CommitteeCache {
     shuffling_start_shard: u64,
     shard_count: u64,
     committee_count: usize,
-    period_committee_count: usize,
     slots_per_epoch: u64,
 }
 
@@ -49,11 +48,6 @@ impl CommitteeCache {
         let committee_count = T::get_epoch_committee_count(
             active_validator_indices.len(),
             spec.target_committee_size,
-        ) as usize;
-
-        let period_committee_count = T::get_epoch_committee_count(
-            active_validator_indices.len(),
-            spec.target_period_committee_size,
         ) as usize;
 
         let shuffling_start_shard =
@@ -172,12 +166,12 @@ impl CommitteeCache {
         })
     }
 
-    pub fn get_period_committee_for_shard(&self, shard: Shard) -> Option<CrosslinkCommittee> {
+    pub fn get_period_committee_for_shard(&self, shard: Shard, committee_size: u64) -> Option<CrosslinkCommittee> {
         let committee = self.get_crosslink_committee_for_shard(shard)?.into_owned();
 
         Some(PeriodCommittee {
             shard,
-            committee[:self.period_committee_count],
+            committee[:committee_size],
         })
     }
 
