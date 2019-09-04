b34f78fa4e3f03dfe6a012c16858d7623f2558dc
Will Villanueva <hello@willvillanueva.com>
2019-09-04 12:52:47 -0400

    Update period committee to have owned/sliced

--- eth2/types/src/period_committee.rs
+++ eth2/types/src/period_committee.rs
@@ -3,8 +3,25 @@ use tree_hash_derive::{CachedTreeHash, TreeHash};
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Default, Clone, Debug, PartialEq, TreeHash, Serialize, Deserialize, CachedTreeHash)]
-pub struct PeriodCommittee {
+pub struct PeriodCommittee<'a> {
     pub period: Period,
+    pub shard: Shard,
+    pub committee: &'a [usize],
+}
+
+impl<'a> PeriodCommittee<'a> {
+    pub fn into_owned(self) -> OwnedPeriodCommittee {
+        OwnedPeriodCommittee {
+            slot: self.slot,
+            shard: self.shard,
+            committee: self.committee.to_vec(),
+        }
+    }
+}
+
+#[derive(Default, Clone, Debug, PartialEq, TreeHash, CachedTreeHash)]
+pub struct OwnedPeriodCommittee {
+    pub slot: Slot,
     pub shard: Shard,
     pub committee: Vec<usize>,
 }
