cec71bfa475e76dccb95f03b7428d35dd0301b8f
Will Villanueva <hello@willvillanueva.com>
2019-09-04 17:34:04 -0400

    Change persistent committee to naturally a owned type

--- eth2/types/src/persistent_committee.rs
+++ eth2/types/src/persistent_committee.rs
@@ -4,22 +4,5 @@ use crate::*;
 pub struct PersistentCommittee<'a> {
     pub epoch: Epoch,
     pub shard: Shard,
-    pub committee: &'a [usize],
-}
-
-impl<'a> PersistentCommittee<'a> {
-    pub fn into_owned(self) -> OwnedPersistentCommittee {
-        OwnedPersistentCommittee {
-            epoch: self.epoch,
-            shard: self.shard,
-            committee: self.committee.to_vec(),
-        }
-    }
-}
-
-#[derive(Default, Clone, Debug, PartialEq)]
-pub struct OwnedPersistentCommittee {
-    pub epoch: Epoch,
-    pub shard: Shard,
     pub committee: Vec<usize>,
 }
