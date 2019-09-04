36ed750179c69cafe6f84aa55963f1c355e96616
Will Villanueva <hello@willvillanueva.com>
2019-09-04 12:47:35 -0400

    Define period committee cache

--- eth2/types/src/beacon_state.rs
+++ eth2/types/src/beacon_state.rs
@@ -16,10 +16,12 @@ use tree_hash::TreeHash;
 use tree_hash_derive::{CachedTreeHash, TreeHash};
 
 pub use self::committee_cache::CommitteeCache;
+pub use self::period_committee_cache::PeriodCommitteeCache;
 pub use beacon_state_types::*;
 
 mod beacon_state_types;
 mod committee_cache;
+mod period_committee_cache;
 mod exit_cache;
 mod pubkey_cache;
 mod tests;
@@ -32,6 +34,7 @@ pub enum Error {
     EpochOutOfBounds,
     SlotOutOfBounds,
     ShardOutOfBounds,
+    NoPeriodBoundary,
     UnknownValidator,
     UnableToDetermineProducer,
     InvalidBitfield,
