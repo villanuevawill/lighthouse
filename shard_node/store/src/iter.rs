e9c6b9d70ece5329c0168180e85716422de0d7ae
Will Villanueva <hello@willvillanueva.com>
2019-09-06 14:11:28 -0400

    Shard block header compile - minor changes to store iter/pending att

--- shard_node/store/src/iter.rs
+++ shard_node/store/src/iter.rs
@@ -1,13 +1,13 @@
 use crate::Store;
 use std::borrow::Cow;
 use std::sync::Arc;
-use types::{ShardBlock, ShardState, ShardStateError, EthSpec, Hash256, Slot};
+use types::{ShardBlock, ShardState, ShardStateError, EthSpec, Hash256, ShardSlot};
 
 #[derive(Clone)]
 pub struct StateRootsIterator<'a, T: EthSpec, U> {
     store: Arc<U>,
     shard_state: Cow<'a, ShardState<T>>,
-    slot: Slot,
+    slot: ShardSlot,
 }
 
 impl<'a, T: EthSpec, U: Store> StateRootsIterator<'a, T, U> {
@@ -67,14 +67,14 @@ pub struct BlockIterator<'a, T: EthSpec, U> {
 
 impl<'a, T: EthSpec, U: Store> BlockIterator<'a, T, U> {
     /// Create a new iterator over all blocks in the given `beacon_state` and prior states.
-    pub fn new(store: Arc<U>, shard_state: &'a ShardState<T>, start_slot: Slot) -> Self {
+    pub fn new(store: Arc<U>, shard_state: &'a ShardState<T>, start_slot: ShardSlot) -> Self {
         Self {
             roots: BlockRootsIterator::new(store, shard_state, start_slot),
         }
     }
 
     /// Create a new iterator over all blocks in the given `beacon_state` and prior states.
-    pub fn owned(store: Arc<U>, shard_state: ShardState<T>, start_slot: Slot) -> Self {
+    pub fn owned(store: Arc<U>, shard_state: ShardState<T>, start_slot: ShardSlot) -> Self {
         Self {
             roots: BlockRootsIterator::owned(store, shard_state, start_slot),
         }
@@ -90,29 +90,16 @@ impl<'a, T: EthSpec, U: Store> Iterator for BlockIterator<'a, T, U> {
     }
 }
 
-/// Iterates backwards through block roots. If any specified slot is unable to be retrieved, the
-/// iterator returns `None` indefinitely.
-///
-/// Uses the `latest_block_roots` field of `BeaconState` to as the source of block roots and will
-/// perform a lookup on the `Store` for a prior `BeaconState` if `latest_block_roots` has been
-/// exhausted.
-///
-/// Returns `None` for roots prior to genesis or when there is an error reading from `Store`.
-///
-/// ## Notes
-///
-/// See [`BestBlockRootsIterator`](struct.BestBlockRootsIterator.html), which has different
-/// `start_slot` logic.
 #[derive(Clone)]
 pub struct BlockRootsIterator<'a, T: EthSpec, U> {
     store: Arc<U>,
     shard_state: Cow<'a, ShardState<T>>,
-    slot: Slot,
+    slot: ShardSlot,
 }
 
 impl<'a, T: EthSpec, U: Store> BlockRootsIterator<'a, T, U> {
     /// Create a new iterator over all block roots in the given `shard_state` and prior states.
-    pub fn new(store: Arc<U>, shard_state: &'a ShardState<T>, start_slot: Slot) -> Self {
+    pub fn new(store: Arc<U>, shard_state: &'a ShardState<T>, start_slot: ShardSlot) -> Self {
         Self {
             store,
             shard_state: Cow::Borrowed(shard_state),
@@ -121,7 +108,7 @@ impl<'a, T: EthSpec, U: Store> BlockRootsIterator<'a, T, U> {
     }
 
     /// Create a new iterator over all block roots in the given `beacon_state` and prior states.
-    pub fn owned(store: Arc<U>, shard_state: ShardState<T>, start_slot: Slot) -> Self {
+    pub fn owned(store: Arc<U>, shard_state: ShardState<T>, start_slot: ShardSlot) -> Self {
         Self {
             store,
             shard_state: Cow::Owned(shard_state),
@@ -131,7 +118,7 @@ impl<'a, T: EthSpec, U: Store> BlockRootsIterator<'a, T, U> {
 }
 
 impl<'a, T: EthSpec, U: Store> Iterator for BlockRootsIterator<'a, T, U> {
-    type Item = (Hash256, Slot);
+    type Item = (Hash256, ShardSlot);
 
     fn next(&mut self) -> Option<Self::Item> {
         if (self.slot == 0) || (self.slot > self.shard_state.slot) {
@@ -143,7 +130,7 @@ impl<'a, T: EthSpec, U: Store> Iterator for BlockRootsIterator<'a, T, U> {
         match self.shard_state.get_block_root(self.slot) {
             Ok(root) => Some((*root, self.slot)),
             Err(ShardStateError::SlotOutOfBounds) => {
-                // Read a `BeaconState` from the store that has access to prior historical root.
+                // Read a `ShardState` from the store that has access to prior historical root.
                 let shard_state: ShardState<T> = {
                     // Load the earliest state from disk.
                     let new_state_root = self.shard_state.get_oldest_state_root().ok()?;
@@ -162,34 +149,15 @@ impl<'a, T: EthSpec, U: Store> Iterator for BlockRootsIterator<'a, T, U> {
     }
 }
 
-/// Iterates backwards through block roots with `start_slot` highest possible value
-/// `<= beacon_state.slot`.
-///
-/// The distinction between `BestBlockRootsIterator` and `BlockRootsIterator` is:
-///
-/// - `BestBlockRootsIterator` uses best-effort slot. When `start_slot` is greater than the latest available block root
-/// on `beacon_state`, returns `Some(root, slot)` where `slot` is the latest available block
-/// root.
-/// - `BlockRootsIterator` is strict about `start_slot`. When `start_slot` is greater than the latest available block root
-/// on `beacon_state`, returns  `None`.
-///
-/// This is distinct from `BestBlockRootsIterator`.
-///
-/// Uses the `latest_block_roots` field of `BeaconState` to as the source of block roots and will
-/// perform a lookup on the `Store` for a prior `BeaconState` if `latest_block_roots` has been
-/// exhausted.
-///
-/// Returns `None` for roots prior to genesis or when there is an error reading from `Store`.
 #[derive(Clone)]
 pub struct BestBlockRootsIterator<'a, T: EthSpec, U> {
     store: Arc<U>,
     shard_state: Cow<'a, ShardState<T>>,
-    slot: Slot,
+    slot: ShardSlot,
 }
 
 impl<'a, T: EthSpec, U: Store> BestBlockRootsIterator<'a, T, U> {
-    /// Create a new iterator over all block roots in the given `beacon_state` and prior states.
-    pub fn new(store: Arc<U>, beacon_state: &'a ShardState<T>, start_slot: Slot) -> Self {
+    pub fn new(store: Arc<U>, beacon_state: &'a ShardState<T>, start_slot: ShardSlot) -> Self {
         let mut slot = start_slot;
         if slot >= shard_state.slot {
             // Slot may be too high.
@@ -207,7 +175,7 @@ impl<'a, T: EthSpec, U: Store> BestBlockRootsIterator<'a, T, U> {
     }
 
     /// Create a new iterator over all block roots in the given `beacon_state` and prior states.
-    pub fn owned(store: Arc<U>, shard_state: ShardState<T>, start_slot: Slot) -> Self {
+    pub fn owned(store: Arc<U>, shard_state: ShardState<T>, start_slot: ShardSlot) -> Self {
         let mut slot = start_slot;
         if slot >= shard_state.slot {
             // Slot may be too high.
@@ -228,7 +196,7 @@ impl<'a, T: EthSpec, U: Store> BestBlockRootsIterator<'a, T, U> {
 }
 
 impl<'a, T: EthSpec, U: Store> Iterator for BestBlockRootsIterator<'a, T, U> {
-    type Item = (Hash256, Slot);
+    type Item = (Hash256, ShardSlot);
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.slot == 0 {
