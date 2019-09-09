bceb39409a7a773e83915755a181c4f08bf5d822
Will Villanueva <hello@willvillanueva.com>
2019-09-09 15:31:06 -0400

    Shard Block is more in line with intended spec.

--- eth2/types/src/shard_block.rs
+++ eth2/types/src/shard_block.rs
@@ -8,9 +8,6 @@ use test_random_derive::TestRandom;
 use tree_hash::{SignedRoot, TreeHash};
 use tree_hash_derive::{CachedTreeHash, SignedRoot, TreeHash};
 
-/// A block of the `ShardChain`.
-///
-/// Spec v0.6.3
 #[derive(
     Debug,
     PartialEq,
@@ -25,48 +22,34 @@ use tree_hash_derive::{CachedTreeHash, SignedRoot, TreeHash};
     SignedRoot,
 )]
 pub struct ShardBlock {
+    pub shard: u64,
     pub slot: ShardSlot,
     pub previous_block_root: Hash256,
     pub state_root: Hash256,
-    pub body: ShardBlockBody,
+    pub attestation: ShardAttestation,
     #[signed_root(skip_hashing)]
     pub signature: Signature,
 }
 
 impl ShardBlock {
-    /// Returns an empty block to be used during genesis.
-    ///
-    /// Spec v0.6.3
-    pub fn empty(spec: &ChainSpec) -> BeaconBlock {
+    pub fn empty(spec: &ChainSpec, shard: u64) -> BeaconBlock {
         ShardBlock {
-            slot: spec.genesis_slot,
+            shard,
+            slot: spec.phase_1_fork_slot,
             previous_block_root: spec.zero_hash,
             state_root: spec.zero_hash,
-            body: ShardBlockBody {
-                graffiti: [0; 32],
-                attestations: vec![],
-            },
+            attestation: ShardAttestation::default(),
             signature: Signature::empty_signature(),
         }
     }
 
-    /// Returns the `signed_root` of the block.
-    ///
-    /// Spec v0.6.3
     pub fn canonical_root(&self) -> Hash256 {
         Hash256::from_slice(&self.signed_root()[..])
     }
 
-    /// Returns a full `ShardBlockHeader` of this block.
-    ///
-    /// Note: This method is used instead of an `Into` impl to avoid a `Clone` of an entire block
-    /// when you want to have the block _and_ the header.
-    ///
-    /// Note: performs a full tree-hash of `self.body`.
-    ///
-    /// Spec v0.6.3
     pub fn block_header(&self) -> ShardBlockHeader {
         ShardBlockHeader {
+            shard: self.shard,
             slot: self.slot,
             previous_block_root: self.previous_block_root,
             state_root: self.state_root,
@@ -75,9 +58,6 @@ impl ShardBlock {
         }
     }
 
-    /// Returns a "temporary" header, where the `state_root` is `spec.zero_hash`.
-    ///
-    /// Spec v0.6.3
     pub fn temporary_block_header(&self, spec: &ChainSpec) -> ShardBlockHeader {
         ShardBlockHeader {
             state_root: spec.zero_hash,
@@ -85,12 +65,4 @@ impl ShardBlock {
             ..self.block_header()
         }
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    ssz_tests!(BeaconBlock);
-    cached_tree_hash_tests!(BeaconBlock);
 }
