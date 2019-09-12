167a2d80129b044cdaa4eb5f05944c6c107d0935
Will Villanueva <hello@willvillanueva.com>
2019-09-12 16:33:14 -0500

    Shard node store functioning and compiling :)

---
+++ shard_node/shard_store/src/block_at_slot.rs
@@ -0,0 +1,54 @@
+use super::*;
+use ssz::{Decode, DecodeError};
+
+fn get_block_bytes<T: Store>(store: &T, root: Hash256) -> Result<Option<Vec<u8>>, Error> {
+    store.get_bytes(ShardBlock::db_column().into(), &root[..])
+}
+
+fn read_slot_from_block_bytes(bytes: &[u8]) -> Result<ShardSlot, DecodeError> {
+    let end = std::cmp::min(ShardSlot::ssz_fixed_len(), bytes.len());
+
+    ShardSlot::from_ssz_bytes(&bytes[0..end])
+}
+
+fn read_previous_block_root_from_block_bytes(bytes: &[u8]) -> Result<Hash256, DecodeError> {
+    let previous_bytes = ShardSlot::ssz_fixed_len();
+    let slice = bytes
+        .get(previous_bytes..previous_bytes + Hash256::ssz_fixed_len())
+        .ok_or_else(|| DecodeError::BytesInvalid("Not enough bytes.".to_string()))?;
+
+    Hash256::from_ssz_bytes(slice)
+}
+
+pub fn get_block_at_preceeding_slot<T: Store>(
+    store: &T,
+    slot: ShardSlot,
+    start_root: Hash256,
+) -> Result<Option<(Hash256, ShardBlock)>, Error> {
+    Ok(match get_at_preceeding_slot(store, slot, start_root)? {
+        Some((hash, bytes)) => Some((hash, ShardBlock::from_ssz_bytes(&bytes)?)),
+        None => None,
+    })
+}
+
+fn get_at_preceeding_slot<T: Store>(
+    store: &T,
+    slot: ShardSlot,
+    mut root: Hash256,
+) -> Result<Option<(Hash256, Vec<u8>)>, Error> {
+    loop {
+        if let Some(bytes) = get_block_bytes(store, root)? {
+            let this_slot = read_slot_from_block_bytes(&bytes)?;
+
+            if this_slot == slot {
+                break Ok(Some((root, bytes)));
+            } else if this_slot < slot {
+                break Ok(None);
+            } else {
+                root = read_previous_block_root_from_block_bytes(&bytes)?;
+            }
+        } else {
+            break Ok(None);
+        }
+    }
+}
---
+++ shard_node/shard_store/src/impls.rs
@@ -0,0 +1,16 @@
+use crate::*;
+use ssz::{Decode, Encode};
+
+impl StoreItem for ShardBlock {
+    fn db_column() -> DBColumn {
+        DBColumn::ShardBlock
+    }
+
+    fn as_store_bytes(&self) -> Vec<u8> {
+        self.as_ssz_bytes()
+    }
+
+    fn from_store_bytes(bytes: &mut [u8]) -> Result<Self, Error> {
+        Self::from_ssz_bytes(bytes).map_err(Into::into)
+    }
+}
