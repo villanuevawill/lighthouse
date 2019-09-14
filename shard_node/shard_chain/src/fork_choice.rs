5fa84006084c104e76768a8dd0f1ab9f9d7ae9da
Will Villanueva <hello@willvillanueva.com>
2019-09-14 11:45:06 -0500

    Shard chain dependencies in check and all is compiling

--- shard_node/shard_chain/src/fork_choice.rs
+++ shard_node/shard_chain/src/fork_choice.rs
@@ -1,3 +1,10 @@
+use crate::{ShardChain, ShardChainTypes};
+use shard_lmd_ghost::LmdGhost;
+use state_processing::common::get_attesting_indices_unsorted;
+use std::sync::Arc;
+use shard_store::{Error as StoreError, Store};
+use types::{Attestation, ShardBlock, ShardState, ShardStateError, Epoch, ShardSpec, Hash256};
+
 type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, PartialEq)]
@@ -10,115 +17,107 @@ pub enum Error {
 }
 
 pub struct ForkChoice<T: ShardChainTypes> {
-    backend: T::ShardLmdGhost,
-    /// Used for resolving the `0x00..00` alias back to genesis.
-    ///
-    /// Does not necessarily need to be the _actual_ genesis, it suffices to be the finalized root
-    /// whenever the struct was instantiated.
+    backend: T::LmdGhost,
     genesis_block_root: Hash256,
 }
 
-impl<T: BeaconChainTypes> ForkChoice<T> {
-    /// Instantiate a new fork chooser.
-    ///
-    /// "Genesis" does not necessarily need to be the absolute genesis, it can be some finalized
-    /// block.
+impl<T: ShardChainTypes> ForkChoice<T> {
     pub fn new(
         store: Arc<T::Store>,
         genesis_block: &ShardBlock,
         genesis_block_root: Hash256,
     ) -> Self {
         Self {
-            backend: T::ShardLmdGhost::new(store, genesis_block, genesis_block_root),
+            backend: T::LmdGhost::new(store, genesis_block, genesis_block_root),
             genesis_block_root,
         }
     }
-
     // general pseudocode here
-    pub fn find_head(&self, chain: &ShardChain<T>) -> Result<Hash256> {
-        let beacon_state = chain.get_beacon_state();
-        let finalized_epoch = beacon_state.finalized_epoch;
-        let start_block_root = chain.get_crosslink(finalized_epoch);
-        let start_block_slot = chain.get_block(start_block_root).slot;
+    // pub fn find_head(&self, chain: &ShardChain<T>) -> Result<Hash256> {
+    //     let beacon_state = chain.get_beacon_state();
+    //     let finalized_epoch = beacon_state.finalized_epoch;
+    //     let start_block_root = chain.get_crosslink(finalized_epoch);
+    //     let start_block_slot = chain.get_block(start_block_root).slot;
 
-        // A function that returns the weight for some validator index.
-        let weight = |validator_index: usize| -> Option<u64> {
-            beacon_state
-                .validator_registry
-                .get(validator_index)
-                .map(|v| v.effective_balance)
-        };
+    //     // A function that returns the weight for some validator index.
+    //     let weight = |validator_index: usize| -> Option<u64> {
+    //         beacon_state
+    //             .validator_registry
+    //             .get(validator_index)
+    //             .map(|v| v.effective_balance)
+    //     };
 
-        self.backend
-            .find_head(start_block_slot, start_block_root, weight)
-            .map_err(Into::into)
-    }
+    //     self.backend
+    //         .find_head(start_block_slot, start_block_root, weight)
+    //         .map_err(Into::into)
+    // }
 
-    /// Process all attestations in the given `block`.
-    ///
-    /// Assumes the block (and therefore it's attestations) are valid. It is a logic error to
-    /// provide an invalid block.
-    pub fn process_block(
-        &self,
-        beacon_state: &BeaconState,
-        block: &ShardBlock,
-        block_root: Hash256,
-    ) -> Result<()> {
-        // Note: we never count the block as a latest message, only attestations.
-        //
-        // I (Paul H) do not have an explicit reference to this, but I derive it from this
-        // document:
-        //
-        // https://github.com/ethereum/eth2.0-specs/blob/v0.7.0/specs/core/0_fork-choice.md
-        for attestation in &block.body.attestations {
-            self.process_attestation_from_block(beacon_state, attestation, block)?;
-        }
+    // /// Process all attestations in the given `block`.
+    // ///
+    // /// Assumes the block (and therefore it's attestations) are valid. It is a logic error to
+    // /// provide an invalid block.
+    // pub fn process_block(
+    //     &self,
+    //     beacon_state: &BeaconState,
+    //     block: &ShardBlock,
+    //     block_root: Hash256,
+    // ) -> Result<()> {
+    //     // Note: we never count the block as a latest message, only attestations.
+    //     //
+    //     // I (Paul H) do not have an explicit reference to this, but I derive it from this
+    //     // document:
+    //     //
+    //     // https://github.com/ethereum/eth2.0-specs/blob/v0.7.0/specs/core/0_fork-choice.md
+    //     for attestation in &block.body.attestations {
+    //         self.process_attestation_from_block(beacon_state, attestation, block)?;
+    //     }
 
-        self.backend.process_block(block, block_root)?;
+    //     self.backend.process_block(block, block_root)?;
 
-        Ok(())
-    }
+    //     Ok(())
+    // }
 
-    fn process_attestation_from_block(
-        &self,
-        beacon_state: &BeaconState<T::EthSpec>,
-        attestation: &Attestation,
-        block: &ShardBlock
-    ) -> Result<()> {
-        // Note: `get_attesting_indices_unsorted` requires that the beacon state caches be built.
-        let validator_indices = get_shard_attesting_indices_unsorted(
-            block.slot,
-            beacon_state,
-            &attestation.data,
-            &attestation.aggregation_bitfield,
-        )?;
+    // fn process_attestation_from_block(
+    //     &self,
+    //     beacon_state: &BeaconState<T::EthSpec>,
+    //     attestation: &Attestation,
+    //     block: &ShardBlock
+    // ) -> Result<()> {
+    //     // Note: `get_attesting_indices_unsorted` requires that the beacon state caches be built.
+    //     let validator_indices = get_shard_attesting_indices_unsorted(
+    //         block.slot,
+    //         beacon_state,
+    //         &attestation.data,
+    //         &attestation.aggregation_bitfield,
+    //     )?;
 
-        let block_hash = attestation.data.target_root;
+    //     let block_hash = attestation.data.target_root;
 
-        if block_hash != Hash256::zero() {
-            for validator_index in validator_indices {
-                self.backend
-                    .process_attestation(validator_index, block_hash, block.slot)?;
-            }
-        }
+    //     if block_hash != Hash256::zero() {
+    //         for validator_index in validator_indices {
+    //             self.backend
+    //                 .process_attestation(validator_index, block_hash, block.slot)?;
+    //         }
+    //     }
 
-        Ok(())
-    }
+    //     Ok(())
+    // }
 
-    /// Inform the fork choice that the given block (and corresponding root) have been finalized so
-    /// it may prune it's storage.
-    ///
-    /// `finalized_block_root` must be the root of `finalized_block`.
-    pub fn process_finalization(
-        &self,
-        finalized_block: &BeaconBlock,
-        finalized_block_root: Hash256,
-    ) -> Result<()> {
-        self.backend
-            .update_finalized_root(finalized_block, finalized_block_root)
-            .map_err(Into::into)
-    }
+    // /// Inform the fork choice that the given block (and corresponding root) have been finalized so
+    // /// it may prune it's storage.
+    // ///
+    // /// `finalized_block_root` must be the root of `finalized_block`.
+    // pub fn process_finalization(
+    //     &self,
+    //     finalized_block: &BeaconBlock,
+    //     finalized_block_root: Hash256,
+    // ) -> Result<()> {
+    //     self.backend
+    //         .update_finalized_root(finalized_block, finalized_block_root)
+    //         .map_err(Into::into)
+    // }
 }
+
 
 impl From<ShardStateError> for Error {
     fn from(e: ShardStateError) -> Error {
--- shard_node/shard_chain/src/lib.rs
+++ shard_node/shard_chain/src/lib.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
-}
+pub mod shard_chain;
+pub mod checkpoint;
+pub mod errors;
+pub mod fork_choice;
+
+pub use self::shard_chain::{ShardChain, ShardChainTypes};
+pub use self::checkpoint::CheckPoint;
+pub use self::errors::{ShardChainError, BlockProductionError};
