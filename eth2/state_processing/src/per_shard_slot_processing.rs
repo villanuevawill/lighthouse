7699f24494670431375b63e560b4e4502814e6fc
Will Villanueva <hello@willvillanueva.com>
2019-09-13 19:18:57 -0500

    Shard Slot Processing used and corrected

--- eth2/state_processing/src/per_shard_slot_processing.rs
+++ eth2/state_processing/src/per_shard_slot_processing.rs
@@ -1,7 +1,8 @@
 use crate::*;
 use types::*;
+use tree_hash::TreeHash;
 
-pub fn per_shard_slot_processing<T: EthSpec>(
+pub fn per_shard_slot_processing<T: ShardSpec>(
     state: &mut ShardState<T>,
     spec: &ChainSpec,
 ) -> Result<(), Error> {
@@ -15,7 +16,23 @@ pub fn per_shard_slot_processing<T: EthSpec>(
         // include period processing here :)
     }
 
+    process_shard_slot(state, spec);
+
     state.slot += 1;
 
     Ok(())
+}
+
+// need to put this in separate directory (process slots)
+fn process_shard_slot<T: ShardSpec>(
+    state: &mut ShardState<T>,
+    spec: &ChainSpec,
+) -> () {
+    let previous_state_root = Hash256::from_slice(&state.tree_hash_root()[..]);
+    let mut depth = 0;
+
+    while (state.slot.as_u64() % u64::pow(2, depth as u32) == 0 as u64) && (depth < T::history_accumulator_depth() as u64) {
+        state.history_accumulator[depth as usize] = previous_state_root;
+        depth += 1;
+    }
 }
