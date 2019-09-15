5d4de7daa9a9f2a8f6fd9cff0d006ad940754268
Will Villanueva <hello@willvillanueva.com>
2019-09-15 01:50:28 -0500

    fork choice gadget completed for attestations and block process

--- eth2/state_processing/src/common/get_attesting_indices.rs
+++ eth2/state_processing/src/common/get_attesting_indices.rs
@@ -36,7 +36,7 @@ pub fn get_attesting_indices<T: EthSpec>(
 pub fn get_shard_attesting_indices<T: EthSpec>(
     shard: Shard,
     state: &BeaconState<T>,
-    attestation_data: &AttestationData,
+    attestation_data: &ShardAttestationData,
     bitfield: &Bitfield,
 ) -> Result<Vec<usize>, BeaconStateError> {
     get_shard_attesting_indices_unsorted(shard, state, attestation_data, bitfield).map(|mut indices| {
@@ -47,19 +47,16 @@ pub fn get_shard_attesting_indices<T: EthSpec>(
 }
 
 /// Returns validator indices which participated in the attestation, unsorted.
-///
-/// Spec v0.6.3
 pub fn get_shard_attesting_indices_unsorted<T: EthSpec>(
     shard: Shard,
     state: &BeaconState<T>,
-    attestation_data: &AttestationData,
+    attestation_data: &ShardAttestationData,
     bitfield: &Bitfield,
 ) -> Result<Vec<usize>, BeaconStateError> {
-    let target_relative_epoch =
-        RelativeEpoch::from_epoch(state.current_epoch(), attestation_data.target_epoch)?;
-
+    let spec = T::default_spec();
+    let target_epoch = attestation_data.target_slot.epoch(spec.slots_per_epoch, spec.shard_slots_per_beacon_slot);
     let committee =
-        state.get_crosslink_committee_for_shard(shard, target_relative_epoch)?;
+        state.get_shard_committee(target_epoch, shard)?;
 
     if !verify_bitfield_length(&bitfield, committee.committee.len()) {
         return Err(BeaconStateError::InvalidBitfield);
